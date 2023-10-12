970b0419761de1e54707cc724f8a8760f65f3495
Anders Nissen <andnis@gmail.com>
2023-10-12 20:41:43 +0200

    Add token lexeme and position

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,12 +2,28 @@ use std::char;
 use vec;
 
 #[derive(Debug)]
-pub enum Token {
+pub enum TokenKind {
     Integer(i32),
     Float(f32),
     Plus,
     SyntaxError,
     NewLine,
+}
+
+#[derive(Debug)]
+pub struct Span {
+    pub line: usize,
+    pub column: usize,
+    // start and end *can* be extracted from the lexeme
+    // start: usize,
+    // length: usize,
+}
+
+#[derive(Debug)]
+pub struct Token<'a> {
+    pub kind: TokenKind,
+    pub position: Span,
+    pub lexeme: &'a str,
 }
 
 struct Lexer {
@@ -18,11 +34,11 @@ struct Lexer {
     column: usize,
 }
 
-pub fn lex(source: &str) -> Vec<Token> {
+pub fn lex<'a>(source: &'a str) -> Vec<Token> {
     Lexer::new().scan_tokens(source)
 }
 
-impl Lexer {
+impl<'a> Lexer {
     fn new() -> Self {
         Self {
             source: Vec::default(),
@@ -33,37 +49,49 @@ impl Lexer {
         }
     }
 
-    fn scan_tokens(&mut self, source: &str) -> Vec<Token> {
+    fn scan_tokens(&mut self, source: &'a str) -> Vec<Token<'a>> {
         self.source = source.chars().collect();
 
         let mut tokens = vec![];
 
         while !self.is_at_end() {
             self.start = self.current;
-            let token = self.scan_token();
-            tokens.push(token); // TODO: Enrich tokens with line and column
+            if let Some(kind) = self.scan_next() {
+                let position = Span {
+                    line: self.line,
+                    column: self.column,
+                    // start: self.start,
+                    // length: self.current - self.start,
+                };
+                let token = Token {
+                    kind,
+                    position,
+                    lexeme: &source[self.start..self.current],
+                };
+                tokens.push(token);
+            }
             self.column += self.current - self.start;
         }
-        tokens.into_iter().flatten().collect()
+        tokens
     }
 
-    fn scan_token(&mut self) -> Option<Token> {
+    fn scan_next(&mut self) -> Option<TokenKind> {
         let char = self.advance();
         let token = match char {
             ' ' => None,
-            '+' => Some(Token::Plus),
+            '+' => Some(TokenKind::Plus),
             '\n' => {
                 self.line += 1;
                 self.column = 0;
-                Some(Token::NewLine)
+                Some(TokenKind::NewLine)
             }
             c if self.is_digit(c) => Some(self.number()),
-            _ => Some(Token::SyntaxError),
+            _ => Some(TokenKind::SyntaxError),
         };
         token
     }
 
-    fn number(&mut self) -> Token {
+    fn number(&mut self) -> TokenKind {
         while self.is_digit(self.peek()) {
             self.advance();
         }
@@ -80,10 +108,10 @@ impl Lexer {
         let str: String = chars.into_iter().collect(); // HACK: This could probably be done better
         if is_float {
             let value = str.parse::<f32>().unwrap();
-            Token::Float(value)
+            TokenKind::Float(value)
         } else {
             let value = str.parse::<i32>().unwrap();
-            Token::Integer(value)
+            TokenKind::Integer(value)
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 mod lexer;
 
 fn main() {
-    let source = "2+1.3 + 4";
+    let source = "2+1.3 xyz 4";
     println!("{}", source);
 
     let tokens = lexer::lex(source);
     println!("=>");
-    tokens.iter().for_each(|f| println!("tokens: {:?}", f));
+    // tokens.iter().for_each(|f| println!("tokens: {:?}", f));
+    tokens.iter().for_each(|f| {
+        match f.kind {
+            lexer::TokenKind::SyntaxError => {
+                println!(
+                    "syntax error at '{}' (line {}, column {})",
+                    f.lexeme, f.position.line, f.position.column,
+                )
+            }
+            _ => println!("tokens: {:?}", f.kind),
+        }
+        // println!("tokens: {:?}", f)
+    });
 }
