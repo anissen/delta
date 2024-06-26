f4fbc998c97827cde392c8e2af8ec4a6ca617781
Anders Nissen <andnis@gmail.com>
2024-06-26 10:20:05 +0200

    refactor: Refactor reading and incrementing program counter in value instructions

--- src/vm.rs
+++ src/vm.rs
@@ -12,7 +12,7 @@ pub enum Value {
 }
 
 struct CallFrame {
-    return_program_counter: usize,
+    return_program_counter: usize, // maybe simply `ip`?
 }
 
 pub struct VirtualMachine {
@@ -53,38 +53,24 @@ impl VirtualMachine {
             println!("Instruction: {:?}", instruction);
             match instruction {
                 ByteCode::PushBoolean => {
-                    let value_bytes = self.program[self.program_counter];
-                    self.program_counter += 1;
+                    let value_bytes = self.read_byte();
                     let value = value_bytes != 0;
                     self.stack.push(Value::Boolean(value));
                 }
 
                 ByteCode::PushInteger => {
-                    let value_bytes: [u8; 4] = self.program
-                        [self.program_counter..self.program_counter + 4]
-                        .try_into()
-                        .unwrap();
-                    self.program_counter += 4;
-                    let raw = i32::from_be_bytes(value_bytes);
-                    self.stack.push(Value::Integer(raw));
+                    let value = self.read_i32();
+                    self.stack.push(Value::Integer(value));
                 }
 
                 ByteCode::PushFloat => {
-                    let value_bytes: [u8; 4] = self.program
-                        [self.program_counter..self.program_counter + 4]
-                        .try_into()
-                        .unwrap();
-                    let raw = u32::from_be_bytes(value_bytes);
-                    let value: f32 = f32::from_bits(raw);
-                    self.program_counter += 4;
+                    let value = self.read_f32();
                     self.push_float(value);
                 }
 
                 ByteCode::Addition => {
                     let right = self.stack.pop().unwrap();
                     let left = self.stack.pop().unwrap();
-                    // println!("{} + {}", left, right);
-                    // self.push_float(left + right);
                     match (right, left) {
                         (Value::Float(right), Value::Float(left)) => self.push_float(left + right),
 
@@ -99,8 +85,6 @@ impl VirtualMachine {
                 ByteCode::Subtraction => {
                     let right = self.stack.pop().unwrap();
                     let left = self.stack.pop().unwrap();
-                    // println!("{} - {}", left, right);
-                    // self.push_float(left - right);
                     match (right, left) {
                         (Value::Float(right), Value::Float(left)) => self.push_float(left - right),
 
@@ -115,8 +99,6 @@ impl VirtualMachine {
                 ByteCode::Multiplication => {
                     let right = self.stack.pop().unwrap();
                     let left = self.stack.pop().unwrap();
-                    // println!("{} - {}", left, right);
-                    // self.push_float(left * right);
                     match (right, left) {
                         (Value::Float(right), Value::Float(left)) => self.push_float(left * right),
 
@@ -131,7 +113,6 @@ impl VirtualMachine {
                 ByteCode::Division => {
                     let right = self.stack.pop().unwrap();
                     let left = self.stack.pop().unwrap();
-                    // println!("{} / {}", left, right);
 
                     // TODO(anissen): Division by zero => 0
                     match (right, left) {
@@ -156,16 +137,14 @@ impl VirtualMachine {
                 }
 
                 ByteCode::GetValue => {
-                    let index = self.program[self.program_counter]; // TODO(anissen): Make helper function to read bytes and increment program counter
-                    self.program_counter += 1;
+                    let index = self.read_byte();
                     println!("index is: {}", index);
                     let value = values.get(&index).unwrap();
                     self.stack.push(*value);
                 }
 
                 ByteCode::SetValue => {
-                    let index = self.program[self.program_counter];
-                    self.program_counter += 1;
+                    let index = self.read_byte();
                     let value = self.stack.pop().unwrap();
                     values.insert(index, value);
                     self.stack.push(value); // TODO(anissen): This could be done with a peek instead of a pop + push
@@ -173,19 +152,16 @@ impl VirtualMachine {
 
                 // https://godbolt.org/#g:!((g:!((g:!((h:codeEditor,i:(filename:'1',fontScale:14,fontUsePx:'0',j:1,lang:scala,selection:(endColumn:1,endLineNumber:16,positionColumn:1,positionLineNumber:16,selectionStartColumn:1,selectionStartLineNumber:16,startColumn:1,startLineNumber:16),source:'@main%0Adef+main()+%3D+%7B%0A++println(%22hello%22)%0A%0A++val+y+%3D+3%0A++def+twice(v:+Float)+%3D+%7B%0A++++v+*+2+%2B+y%0A++%7D%0A%0A++println(%22world%22)%0A%0A++val+x+%3D+twice(5)%0A%0A++println(x)%0A%7D%0A'),l:'5',n:'1',o:'Scala+source+%231',t:'0')),k:50,l:'4',n:'0',o:'',s:0,t:'0'),(g:!((h:compiler,i:(compiler:scalac300,filters:(b:'0',binary:'1',binaryObject:'1',commentOnly:'0',debugCalls:'1',demangle:'0',directives:'0',execute:'1',intel:'0',libraryCode:'0',trim:'1',verboseDemangling:'0'),flagsViewOpen:'1',fontScale:14,fontUsePx:'0',j:1,lang:scala,libs:!(),options:'',overrides:!(),selection:(endColumn:14,endLineNumber:65,positionColumn:14,positionLineNumber:65,selectionStartColumn:14,selectionStartLineNumber:65,startColumn:14,startLineNumber:65),source:1),l:'5',n:'0',o:'+scalac+3.0.0+(Editor+%231)',t:'0')),k:50,l:'4',n:'0',o:'',s:0,t:'0')),l:'2',n:'0',o:'',t:'0')),version:4
                 ByteCode::FunctionStart => {
-                    let function_index = self.program[self.program_counter];
-                    self.program_counter += 1;
+                    let function_index = self.read_byte();
 
-                    let param_count = self.program[self.program_counter];
-                    self.program_counter += 1;
+                    let param_count = self.read_byte();
                     for param in 0..param_count {
                         // ???
                     }
 
                     // jump to function end HACK!
                     while self.program_counter < self.program.len() {
-                        let instruction = self.program[self.program_counter];
-                        self.program_counter += 1;
+                        let instruction = self.read_byte();
                         if let Ok(ByteCode::FunctionEnd) = ByteCode::try_from(instruction) {
                             break;
                         }
@@ -215,6 +191,31 @@ impl VirtualMachine {
     // fn call(&mut self) {
     //    // ...
     // }
+    //
+
+    fn read_byte(&mut self) -> u8 {
+        let byte = self.program[self.program_counter];
+        self.program_counter += 1;
+        byte
+    }
+
+    fn read_4bytes(&mut self) -> [u8; 4] {
+        let value_bytes: [u8; 4] = self.program[self.program_counter..self.program_counter + 4]
+            .try_into()
+            .unwrap();
+        self.program_counter += 4;
+        value_bytes
+    }
+
+    fn read_i32(&mut self) -> i32 {
+        let raw = self.read_4bytes();
+        i32::from_be_bytes(raw)
+    }
+
+    fn read_f32(&mut self) -> f32 {
+        let raw = u32::from_be_bytes(self.read_4bytes());
+        f32::from_bits(raw)
+    }
 
     fn pop_boolean(&mut self) -> bool {
         match self.stack.pop().unwrap() {
