547102818507b5dd0f379fbee6ccbe60bde1d917
Anders Nissen <andnis@gmail.com>
2024-12-08 20:05:46 +0100

    Fix bug w. EOF

--- src/parser.rs
+++ src/parser.rs
@@ -319,6 +319,8 @@ impl Parser {
     fn whitespace(&mut self) -> Result<Option<Expr>, String> {
         if self.matches(&[NewLine, Comment]) {
             Ok(None)
+        } else if (self.is_at_end()) {
+            Ok(None)
         } else {
             let error = format!(
                 "Parse error of kind {:?} at {:?} ({:?})",
