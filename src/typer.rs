fdc2d51d214f8b10876e6627351e45360d223f13
Anders Nissen <andnis@gmail.com>
2025-05-21 11:34:04 +0200

    temp

--- src/typer.rs
+++ src/typer.rs
@@ -22,6 +22,12 @@ pub enum Type {
     },
 }
 
+// https://github.com/abs0luty/type_inference_in_rust/blob/main/src/main.rs
+
+// impl Type {
+//     pub fn unify(&self, ) {}
+// }
+
 impl fmt::Display for Type {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let type_name = match self {
@@ -94,6 +100,9 @@ impl<'a> Typer<'a> {
         let mut typ = Type::Unknown;
         for expr in expressions {
             typ = self.type_expr(expr, env, diagnostics);
+            println!("-------------------");
+            println!("{expr:?}");
+            println!("===> has type {typ:?}");
         }
         typ
     }
@@ -157,16 +166,35 @@ impl<'a> Typer<'a> {
                 args,
                 positions,
             } => {
-                let function_type = env.identifiers.get_mut(name).unwrap();
+                let function_type = env.identifiers.get(name).unwrap();
                 match function_type.clone() {
                     Type::Function {
                         parameters,
                         return_type,
                     } => {
-                        for (index, arg) in args.iter().enumerate() {
-                            let parameter = parameters[index].clone();
-                            let position = positions[index].clone();
-                            self.expect_type(arg, *parameter, &position, env, diagnostics);
+                        let mut arg_env = env.clone();
+                        let arg_types = args
+                            .iter()
+                            .map(|arg| Box::new(self.type_expr(&arg, &mut arg_env, diagnostics)))
+                            .collect();
+
+                        let actual = Type::Function {
+                            parameters: arg_types,
+                            return_type: Box::new(Type::Unknown),
+                        };
+
+                        let unified = self.unify(function_type.clone(), actual.clone());
+                        match unified {
+                            Ok(t) => {
+                                env.identifiers.insert(name.clone(), t);
+                            }
+                            Err(err) => {
+                                self.error(
+                                    format!("Expected {} but found {}", function_type, actual),
+                                    positions.first().unwrap().clone(),
+                                    diagnostics,
+                                );
+                            }
                         }
 
                         *return_type.clone()
@@ -349,10 +377,10 @@ impl<'a> Typer<'a> {
     }
 
     // TODO: Move to Type impl?
-    fn unify(&self, expected: Type, actual: Type) -> Type {
-        match (&expected, &actual) {
-            (Type::Any, _) => actual,
-            (Type::Unknown, _) => actual,
+    fn unify(&self, t1: Type, t2: Type) -> Result<Type, String> {
+        match (&t1, &t2) {
+            (Type::Any, _) => Ok(t2),
+            (Type::Unknown, _) => Ok(t2),
             (
                 Type::Function {
                     parameters: expected_parameters,
@@ -365,17 +393,26 @@ impl<'a> Typer<'a> {
             ) => {
                 let mut concrete_parameters = Vec::new();
                 for (expected, actual) in expected_parameters.iter().zip(actual_parameters.iter()) {
-                    let concrete_parameter = self.unify(*expected.clone(), *actual.clone());
+                    let concrete_parameter = self.unify(*expected.clone(), *actual.clone())?;
                     concrete_parameters.push(Box::new(concrete_parameter));
                 }
                 let concrete_return_type =
-                    self.unify(*expected_return_type.clone(), *actual_return_type.clone());
-                Type::Function {
+                    self.unify(*expected_return_type.clone(), *actual_return_type.clone())?;
+                Ok(Type::Function {
                     parameters: concrete_parameters,
                     return_type: Box::new(concrete_return_type),
+                })
+            }
+            _ => {
+                if t1 == t2 {
+                    Ok(t1)
+                } else {
+                    println!("*** FAILED TO UNIFY!");
+                    println!("*** t1 = {t1:?}");
+                    println!("*** t2 = {t2:?}");
+                    Err("failed to unify".to_string())
                 }
             }
-            _ => expected,
         }
     }
 
@@ -385,8 +422,8 @@ impl<'a> Typer<'a> {
     // that has the substitution { x ↦ 1, y ↦ 2 } as its only solution.
 
     fn is_same_or_more_concrete(&self, expected: Type, actual: Type) -> bool {
-        let expected_concrete = self.get_concrete_type(expected.clone(), actual.clone());
-        match (&expected_concrete, &actual) {
+        // let expected_concrete = self.unify(expected.clone(), actual.clone());
+        match (&expected, &actual) {
             (Type::Any, _) | (Type::Unknown, _) => true,
             (
                 Type::Function {
@@ -398,6 +435,10 @@ impl<'a> Typer<'a> {
                     return_type: actual_return_type,
                 },
             ) => {
+                // println!("comparing functions");
+                // dbg!(&expected, &actual);
+                // dbg!(&expected_parameters, &actual_parameters);
+                // dbg!(&expected_return_type, &actual_return_type);
                 if expected_parameters.len() != actual_parameters.len() {
                     false
                 } else if !self.is_same_or_more_concrete(
@@ -406,9 +447,13 @@ impl<'a> Typer<'a> {
                 ) {
                     false
                 } else {
+                    // dbg!(&expected, &actual);
+                    // dbg!(&expected_parameters, &actual_parameters);
                     for (expected, actual) in
                         expected_parameters.iter().zip(actual_parameters.iter())
                     {
+                        // println!("comparing function parameters");
+                        // dbg!(&expected, &actual);
                         if !self.is_same_or_more_concrete(*expected.clone(), *actual.clone()) {
                             return false;
                         }
@@ -416,7 +461,7 @@ impl<'a> Typer<'a> {
                     true
                 }
             }
-            _ => expected_concrete == actual,
+            _ => expected == actual,
         }
     }
 
@@ -429,10 +474,12 @@ impl<'a> Typer<'a> {
         diagnostics: &mut Diagnostics,
     ) {
         let actual_type = self.type_expr(expression, env, diagnostics);
-        if actual_type != Type::Unknown
-            && expected_type != Type::Any
-            && actual_type != expected_type
-        {
+        // let unified_type = self.unify(expected_type.clone(), actual_type.clone());
+
+        if !self.is_same_or_more_concrete(expected_type.clone(), actual_type.clone()) {
+            dbg!(&actual_type);
+            // dbg!(&env);
+            // dbg!(self.unify(expected_type.clone(), actual_type.clone()));
             self.error(
                 format!("Expected {} but found {}", expected_type, actual_type),
                 position.clone(),
